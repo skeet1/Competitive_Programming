eb48e0fbfba0e2e64931b3c71230e23c0d95ff59
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-01-05 19:17:12 +0100

    solve stones on the table problem

--- level_0/stones_on_the_table.cpp
+++ level_0/stones_on_the_table.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	int	len;
-	cin >> len;
-	char	s[len+1];
-	cin.getline(s, len+1);
-
-	int	i = 1;
-	int min = 0;
-	while (i < len+1)
-	{
-		if (s[i] == s[i-1])
-			min++;
-		i++;
-	}
-	cout << min;
+	int	n; cin >> n;
+	int r = 0;
+	string s; cin >> s;
+	for (int i = 1; i < n; i++)
+		if (s[i] == s[i - 1])
+			r++;
+	cout << r << endl;
 }
