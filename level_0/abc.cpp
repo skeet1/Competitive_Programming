844a47d7e39a2e2014dd8b28ee372c3047614b1f
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2022-01-31 12:44:55 +0100

    add some cp books

---
+++ level_0/abc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		int l; cin >> l;
+		string s; cin >> s;
+		if (l == 1) cout << "YES" << endl;
+		else if (l == 2)
+			s[0] == s[1] ? cout << "NO" << endl : cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+}
