c06e8c5a0ee0126a5a37cbcdcb40aa85fa6a6c93
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-02-11 15:04:28 +0100

    add

---
+++ A/borze.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == '.')
+			cout << 0;
+		else if (i < s.length() - 1)
+		{
+			if (s[i] == '-' && s[i + 1] == '.')
+				cout << 1;
+			else if (s[i] == '-' && s[i + 1] == '-')
+				cout << 2;
+			i++;
+		}
+	}
+	cout << endl;
+}
