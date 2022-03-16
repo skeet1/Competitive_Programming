d3cef5b74d39f478a5f1262d50e292fc3c9cee7a
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-03-16 11:12:12 +0100

    add

--- cses/Introductory Problems/number_spiral.cpp
+++ cses/Introductory Problems/number_spiral.cpp
@@ -1,20 +1,55 @@
+/* SKEET IS HERE */
+
 #include <iostream>
+#include <vector>
 #include <algorithm>
 #include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <map>
 
 using namespace std;
-int main()
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+#define F first
+#define nl '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define L length()
+
+void	solve()
 {
-	int t; cin >> t;
-	while (t--)
+	int x, y; cin >> x >> y;
+	if (x > y)
 	{
-		long long x, y;
-		cin >> x >> y;
-		long long res = max(x, y) * max(x, y);
-		if (min(x, y) == y && y % 2 == 1)
-			res -= x * 2 - y - 1;
+		if (x % 2)
+			cout << (x - 1) * (x - 1) + y << nl;
 		else
-			res -= x - 1;
-		cout << res << '\n';
+			cout << x * x - y + 1 << nl;
 	}
+	else
+	{
+		if (y % 2)
+			cout << y * y - x + 1 << nl;
+		else
+			cout << (y - 1) * (y - 1) + x << nl;
+	}
+}
+
+signed main()
+{
+    int t;
+	cin >> t;
+    while(t--)
+    {
+        solve();
+    }
 }
