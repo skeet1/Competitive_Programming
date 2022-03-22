929f530ffd19383201ba938aa46f767e29884977
Mohamed Karim <76565545+skeet1@users.noreply.github.com>
2022-03-22 12:04:39 +0100

    add

--- A/petya_and_strings.cpp
+++ A/petya_and_strings.cpp
@@ -1,29 +1,57 @@
+/* SKEET IS HERE */
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstring>
+#include <cmath>
 #include <string>
+#include <cstring>
+#include <iomanip>
+#include <map>
 
 using namespace std;
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+#define F first
+#define nl '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define L length()
 
-int main()
+void	solve()
 {
-	string s1; cin >> s1;
-	string s2; cin >> s2;
-	for (int i = 0; i < s1.length(); i++)
-		s1[i] = tolower(s1[i]);
-	for (int i = 0; i < s2.length(); i++)
-		s2[i] = tolower(s2[i]);
-	int r = 0;
+	string s1, s2; cin >> s1 >> s2;
 	for (int i = 0; i < s1.length(); i++)
 	{
-		r = s1[i] - s2[i];
-		if (r != 0)
-			goto B;
+		s1[i] = tolower(s1[i]);
+		s2[i] = tolower(s2[i]);
 	}
-B:	if (r < 0)
-		r = -1;
-	if (r > 0)
-		r = 1;
- 	cout << r << endl;	
+	int ans = 0;
+	for (int i = 0; i < s1.length(); i++)
+		if (s1[i] != s2[i])
+		{
+			if (s2[i] > s1[i]) ans = -1;
+			else ans = 1;
+			break;
+		}
+	cout << ans << nl;
+}
+
+signed main()
+{
+    int t;
+//    cin >> t;
+	t = 1;
+    while(t--)
+    {
+        solve();
+    }
 }
